1dfad7d64e41bb0ac003f8480e6889b0c9407ea3
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-05-03 18:57:44 -0400

    Broadcasting
    m*n + m*1 equivalent to m*n + m*n

---
+++ Broadcasting_Wk2_Lecture.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import numpy as np
+
+A = np.array([[56, 9, 4.4, 68],
+              [1.2, 104, 52, 8],
+              [1.8, 135, 99, 0.9]])
+
+print(A)
+
+cal = A.sum(axis=0)
+print(cal)
+
+percentage = 100 * A/cal.reshape(1,4)
+print(percentage)
+
+
+# In[ ]:
+
+
+
+
